55eda5c304274bdbeed889187a04fc5c4105d55c
kai <kaizeng@eecs.berkeley.edu>
2015-05-26 14:26:02 -0700

    add profiling

---
+++ scripts/set-profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+YJP=`ls /root/ | grep yjp`
+echo "SPARK_DAEMON_JAVA_OPTS+=' -agentpath:/root/$YJP/bin/linux-x86-64/libyjpagent.so=sampling'" >> ~/spark/conf/spark-env.sh
+echo "export SPARK_DAEMON_JAVA_OPTS" >> ~/spark/conf/spark-env.sh
+echo "SPARK_JAVA_OPTS+=' -agentpath:/root/$YJP/bin/linux-x86-64/libyjpagent.so=sampling'" >> ~/spark/conf/spark-env.sh
+echo "export SPARK_JAVA_OPTS" >> ~/spark/conf/spark-env.sh
+
+~/spark-ec2/copy-dir --delete ~/spark/conf/
+cp ~/spark/conf/* /mnt/bootstrap-sql/conf/
+
+/mnt/demo-kit/scripts/kill-all.sh && \
+/mnt/demo-kit/scripts/start-all.sh
